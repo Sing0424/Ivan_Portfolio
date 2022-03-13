5f0279b4439c3e14df7117f1660b1982406104c0
Sing0424 <ivan4680@gmail.com>
2022-03-13 19:49:32 +0800

    spell fix

--- src/component/App/App.js
+++ src/component/App/App.js
@@ -2,22 +2,22 @@ import Home from "../Home/Home";
 import Nav from "../Nav/Nav";
 import Resume from "../Resume/Resume";
 import Collection from "../Collection/Collection";
-import Contact from '../Contact/Contact';
-import { Routes, Route } from 'react-router-dom';
-import './App.css';
+import Contact from "../Contact/Contact";
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
     <div className="app">
       <Nav />
       <Routes basename="/Ivan_Portfolio">
-        <Route path="/Ivan_Portfolio" element={<Home/>} />
-        <Route path="/Ivan_Portfolio/Resume" element={<Resume/>} />
-        <Route path="/Ivan_Portfolio/Collection" element={<Collection/>} />
-        <Route path="/Ivan_Portfolio/Contact" element={<Contact/>} />
+        <Route path="/Ivan_Portfolio" element={<Home />} />
+        <Route path="/Ivan_Portfolio/Resume" element={<Resume />} />
+        <Route path="/Ivan_Portfolio/Collection" element={<Collection />} />
+        <Route path="/Ivan_Portfolio/Contact" element={<Contact />} />
       </Routes>
     </div>
-  )
+  );
 }
 
 export default App;
--- src/component/Nav/Nav.js
+++ src/component/Nav/Nav.js
@@ -1,48 +1,80 @@
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
-import './Nav.css';
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Nav.css";
 
 export default function Nav() {
-    const [homeActive, setHomeActive] = useState('active');
-    const [resumeActive, setResumeActive] = useState('unactive');
-    const [collectionActive, setCollectionActive] = useState('unactive');
-    const [contactActive, setContactActive] = useState('unactive');
+  const [homeActive, setHomeActive] = useState("active");
+  const [resumeActive, setResumeActive] = useState("inactive");
+  const [collectionActive, setCollectionActive] = useState("inactive");
+  const [contactActive, setContactActive] = useState("inactive");
 
-    const handleHome = () => {
-        setHomeActive('active')
-        setResumeActive('unactive')
-        setCollectionActive('unactive')
-        setContactActive('unactive')
-    }
-    const handleResume = () => {
-        setHomeActive('unactive')
-        setResumeActive('active')
-        setCollectionActive('unactive')
-        setContactActive('unactive')
-    }
-    const handleCollection = () => {
-        setHomeActive('unactive')
-        setResumeActive('unactive')
-        setCollectionActive('active')
-        setContactActive('unactive')
-    }
-    const handleContact = () => {
-        setHomeActive('unactive')
-        setResumeActive('unactive')
-        setCollectionActive('unactive')
-        setContactActive('active')
-    }
+  const handleHome = () => {
+    setHomeActive("active");
+    setResumeActive("inactive");
+    setCollectionActive("inactive");
+    setContactActive("inactive");
+  };
+  const handleResume = () => {
+    setHomeActive("inactive");
+    setResumeActive("active");
+    setCollectionActive("inactive");
+    setContactActive("inactive");
+  };
+  const handleCollection = () => {
+    setHomeActive("inactive");
+    setResumeActive("inactive");
+    setCollectionActive("active");
+    setContactActive("inactive");
+  };
+  const handleContact = () => {
+    setHomeActive("inactive");
+    setResumeActive("inactive");
+    setCollectionActive("inactive");
+    setContactActive("active");
+  };
 
-    return (
-        <div className='navBar'>
-            <nav>
-                <ul>
-                    <li><Link onClick={handleHome} className={homeActive} to="/Ivan_Portfolio">Home</Link></li>
-                    <li><Link onClick={handleResume} className={resumeActive} to="/Ivan_Portfolio/Resume">Resume</Link></li>
-                    <li><Link onClick={handleCollection} className={collectionActive} to="/Ivan_Portfolio/Collection">Collection</Link></li>
-                    <li><Link onClick={handleContact} className={contactActive} to="/Ivan_Portfolio/Contact">Contact</Link></li>
-                </ul>
-            </nav>
-        </div>
-    )
+  return (
+    <div className="navBar">
+      <nav>
+        <ul>
+          <li>
+            <Link
+              onClick={handleHome}
+              className={homeActive}
+              to="/Ivan_Portfolio"
+            >
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              onClick={handleResume}
+              className={resumeActive}
+              to="/Ivan_Portfolio/Resume"
+            >
+              Resume
+            </Link>
+          </li>
+          <li>
+            <Link
+              onClick={handleCollection}
+              className={collectionActive}
+              to="/Ivan_Portfolio/Collection"
+            >
+              Collection
+            </Link>
+          </li>
+          <li>
+            <Link
+              onClick={handleContact}
+              className={contactActive}
+              to="/Ivan_Portfolio/Contact"
+            >
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 }
