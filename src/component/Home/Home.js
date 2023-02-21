dc1457cacf8f87a681bc14d5c997323dd7ea5727
Sing0424 <ivan4680@gmail.com>
2023-02-22 01:34:55 +0800

    profilo update

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -1,36 +1,62 @@
-import './Home.css';
-import ProfilePic from '../../images/ProfilePic.jpg';
-import Github from '../../images/github_icon.svg';
-import LinkedIn from '../../images/linkedin_icon.svg';
+import "./Home.css";
+import ProfilePic from "../../images/ProfilePic.jpg";
+import Github from "../../images/github_icon.svg";
+import LinkedIn from "../../images/linkedin_icon.svg";
 
 export default function Home() {
-    return (
-        <div className='home'>
-            <title>Home | Ivan's portfolio</title>
-            <section className='left'>
-                <div className='title'>
-                    <h1>
-                        Hi, I am <br/>
-                        Ivan Tung
-                    </h1>
-                    <div className='icon'>
-                    <a className='github' href='https://github.com/Sing0424' target="_blank" rel="noopener noreferrer"><img className='githubicon' src={Github} alt='github' /></a>
-                    <a className='linkedin' href='https://www.linkedin.com/in/tat-sing-tung-7400b9227/' target="_blank" rel="noopener noreferrer"><img className='linkedinicon' src={LinkedIn} alt='linkedin' /></a>
-                    </div>
-                </div>
-                <p>
-                    I am a front-end web developer after graduating from the Generation X CLAP@JC Front-end Web Developer Program funded by the Hong Kong Jockey Club.
-                </p>
-                <p>
-                    Before that, I graduated from Bachelor of Communication Art, Chaoyang University of Technology, as well as Higher Diploma of Multimedia: Information of Technology, Institute of Vocational Education.
-                </p>
-                <p>
-                    I have developed a proficient understanding of web technology and worked on projects using HTML, CSS, Javascript, Github and React. I hope to combine the skills gained in my design, photography and digital marketing experience and technical knowledge to build a web developer career.
-                </p>
-            </section>
-            <section className='right'>
-                <img src={ProfilePic} alt='IvanPic' />
-            </section>
+  return (
+    <div className="home">
+      <title>Home | Ivan's portfolio</title>
+      <section className="left">
+        <div className="title">
+          <h1>
+            Hi, I am <br />
+            Ivan Tung
+          </h1>
+          <div className="icon">
+            <a
+              className="github"
+              href="https://github.com/Sing0424"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img className="githubicon" src={Github} alt="github" />
+            </a>
+            <a
+              className="linkedin"
+              href="https://www.linkedin.com/in/tat-sing-tung-7400b9227/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img className="linkedinicon" src={LinkedIn} alt="linkedin" />
+            </a>
+          </div>
         </div>
-    )
+        <p>
+          I am a web developer after graduating from the Generation X CLAP@JC
+          Front-end Web Developer Program funded by the Hong Kong Jockey Club.
+        </p>
+        <p>
+          Before that, I graduated from Bachelor of Communication Art, Chaoyang
+          University of Technology, as well as Higher Diploma of Multimedia:
+          Information of Technology, Institute of Vocational Education.
+        </p>
+        <p>
+          In 2022 to 2023, I worked in ITPRO2 Limited as a Junior web Developer,
+          owned program development, data analysis and also testing related from
+          MYSQL, Python, .NET(C#) and Javascript
+        </p>
+        <p>
+          I have developed a proficient understanding of web technology and
+          worked on projects using Python, SQL, HTML, CSS, Javascript, Github
+          and React. I hope to combine the skills gained in my design,
+          photography and digital marketing experience and technical knowledge
+          to build a web developer career.
+        </p>
+      </section>
+      <section className="right">
+        <img src={ProfilePic} alt="IvanPic" />
+      </section>
+    </div>
+  );
 }
--- src/component/Resume/Resume.js
+++ src/component/Resume/Resume.js
@@ -1,85 +1,132 @@
-import './Resume.css';
+import "./Resume.css";
 
 export default function Resume() {
-    return (
-        <div className="resume">
-        <title>Resume | Ivan's portfolio</title>
-            <div className='resumeInner'>
-                <h1 className='resumeTitle'>Introduction</h1>
-                <p>
-                    Passionate and innovative junior front-end web developer seeking entry level positions in the areas of the front-end and user experience design using Javascript, HTML5, and React or similar Javascript framework for mobile websites and applications development.
-                </p>
-                <h1 className='resumeTitle'>Experience </h1>
-                <div className='listDiv'>
-                    <ul>
-                        <li><p>Creative Design and Photographer</p></li>
-                        <li><p>Digital Marketing Specialist / Photographer in KBLHK (Amateur Basketball League)</p></li>
-                        <li><p>VR & AR School Projects</p></li>
-                        <li><p>Todo List WebApp School Project</p></li>
-                    </ul>
-                </div>
-                <h1 className='resumeTitle'>Education & Training</h1>
-                <p>
-                    Front-End Web Developer Program
-                    <br/>
-                    Generation x CLAP@JC
-                    <br/>
-                    <br/>
-                    Bachelor of Communication Art : Communication Art
-                    <br/>
-                    Chaoyang University of Technology
-                    <br/>
-                    TaiChung, Taiwan
-                    <br/>
-                    <br/>
-                    Higher Diploma of Multimedia: Information of Technology
-                    <br/>
-                    Hong Kong Institute of Vocational Education
-                </p>
-                <h1 className='resumeTitle'>Skills</h1>
-                
-                    <div className='skills'>
-                        <div className='ProgrammingSkills'>
-                            <h4>Programming skills</h4>
-                            <div className='listDiv'>
-                                <ul>
-                                    <li><p>HTML</p></li>
-                                    <li><p>CSS</p></li>
-                                    <li><p>Javascript</p></li>
-                                    <li><p>Github</p></li>
-                                    <li><p>React</p></li>
-                                </ul>
-                        </div>
-                        </div>
-                        <div className='AdobeSoftware'>
-                            <h4>Adobe Software</h4>
-                            <div className='listDiv'>
-                                <ul>
-                                        <li><p>Photoshop</p></li>
-                                        <li><p>Lightroom</p></li>
-                                        <li><p>illustrator</p></li>
-                                        <li><p>Premiere Pro</p></li>
-                                        <li><p>AfterEffect</p></li>
-                                    </ul>    
-                            </div>
-                        </div>
-                    </div>
-                
-                <h1 className='resumeTitle'>Languages</h1>
-                <p>
-                Native: Cantonese
-                <br/>
-                Proficient: Mandarin
-                <br/>
-                Conversant: English
-                </p>
-                <h1 className='resumeTitle'>Certification</h1>
-                <p>
-                LCCI - HKVEP English Programme (Writing & Speaking)
-                <br/>
-                Benchmark Level: 4C
-                </p>
-            </div>
+  return (
+    <div className="resume">
+      <title>Resume | Ivan's portfolio</title>
+      <div className="resumeInner">
+        <h1 className="resumeTitle">Introduction</h1>
+        <p>
+          Passionate and innovative programmer seeking entry level positions in
+          the areas of the programmer using MSSQL, Python, Javascript, HTML5,
+          and React JS or similar Javascript framework for mobile websites or
+          applications development.
+        </p>
+        <h1 className="resumeTitle">Experience </h1>
+        <div className="listDiv">
+          <ul>
+            <li>
+              <p>ITPro2 Limited (Junior web developer) Mar 2022 to Recently</p>
+            </li>
+
+            <li>
+              <p>
+                Photobi.HK (Creative Design and Photographer) Mar 2021 to Sep
+                2021
+              </p>
+            </li>
+            <li>
+              <p>VR & AR School Projects</p>
+            </li>
+            <li>
+              <p>Todo List WebApp School Project</p>
+            </li>
+          </ul>
         </div>
-    )
+        <h1 className="resumeTitle">Education & Training</h1>
+        <p>
+          Front-End Web Developer Program
+          <br />
+          Generation x CLAP@JC
+          <br />
+          <br />
+          Bachelor of Communication Art : Communication Art
+          <br />
+          Chaoyang University of Technology
+          <br />
+          TaiChung, Taiwan
+          <br />
+          <br />
+          Higher Diploma of Multimedia: Information of Technology
+          <br />
+          Hong Kong Institute of Vocational Education
+        </p>
+        <h1 className="resumeTitle">Skills</h1>
+
+        <div className="skills">
+          <div className="ProgrammingSkills">
+            <h4>Programming skills</h4>
+            <div className="listDiv">
+              <ul>
+                <li>
+                  <p>HTML</p>
+                </li>
+                <li>
+                  <p>CSS</p>
+                </li>
+                <li>
+                  <p>Javascript</p>
+                </li>
+                <li>
+                  <p>Github</p>
+                </li>
+                <li>
+                  <p>React</p>
+                </li>
+                <li>
+                  <p>Python</p>
+                </li>
+                <li>
+                  <p>.NET</p>
+                </li>
+                <li>
+                  <p>MS SQL</p>
+                </li>
+                <li>
+                  <p>C#</p>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="AdobeSoftware">
+            <h4>Adobe Software</h4>
+            <div className="listDiv">
+              <ul>
+                <li>
+                  <p>Photoshop</p>
+                </li>
+                <li>
+                  <p>Lightroom</p>
+                </li>
+                <li>
+                  <p>illustrator</p>
+                </li>
+                <li>
+                  <p>Premiere Pro</p>
+                </li>
+                <li>
+                  <p>AfterEffect</p>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <h1 className="resumeTitle">Languages</h1>
+        <p>
+          Native: Cantonese
+          <br />
+          Proficient: Mandarin
+          <br />
+          Conversant: English
+        </p>
+        <h1 className="resumeTitle">Certification</h1>
+        <p>
+          LCCI - HKVEP English Programme (Writing & Speaking)
+          <br />
+          Benchmark Level: 4C
+        </p>
+      </div>
+    </div>
+  );
 }
